615ed2f3f761e34bff3553bd2251086d33f03e06
Akshay Lavan <133519950+akshaylavan@users.noreply.github.com>
2025-06-12 20:13:37 +0530

    Added CSS Style to the Doctor  Page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -26,15 +26,15 @@ const applyFilter = () => {
   return (
     <>
       <div>
-        <p>Browse through the doctors specialist.</p>
-        <div>
-          <div>
-            <p>General physician</p>
-            <p>Gynecologist</p>
-            <p>Dermatologist</p>
-            <p>Pediatricians</p>
-            <p>Neurologist</p>
-            <p>Gastroenterologist</p>
+        <p className='text-gray-500'>Browse through the doctors specialist.</p>
+        <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+          <div className='flex-col gap-4 text-sm text-gray-600'>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>General physician</p>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>Gynecologist</p>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>Dermatologist</p>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>Pediatricians</p>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>Neurologist</p>
+            <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer `}>Gastroenterologist</p>
           </div>
           <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
             {
