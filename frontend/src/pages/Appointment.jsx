0b967ad387b07ef75ad52a424c9bdcc95b0798af
Akshay Lavan <133519950+akshaylavan@users.noreply.github.com>
2025-06-13 15:38:03 +0530

    Appointment page based on the doctor id

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { useEffect, useState } from 'react'
+import { assets } from '../assets/assets'
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams();
+  const {doctors} = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  }
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return docInfo && (
     <>
-    <div>Appointment</div>
+    <div>
+      {/*------------------Docctor Details------------------*/}
+      <div>
+        <div> 
+          <img src={docInfo.image} alt="" />
+        </div>
+
+        <div>
+          {/*------------------DocInfo: name, degree, experience-----------------*/}
+          <p>
+            {docInfo.name} <img src={assets.verified_icon} alt="Verified" />
+            </p>
+            <div>
+              <p>{docInfo.degree} - {docInfo.speciality}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+
     </>
   )
 }
