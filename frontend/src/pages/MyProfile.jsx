9fbf68991bed54e287ad13ccd4c6908d575fde91
Akshay Lavan <133519950+akshaylavan@users.noreply.github.com>
2025-07-20 16:39:43 +0530

    Profile Page Initial Commit

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { useState } from 'react'
+import {assets} from '../assets/assets'
 const MyProfile = () => {
+
+  const [userData,setUserData] = useState({
+    name:"Akshay Lavan",
+    image:assets.profile_pic,
+    email:'akshaylavan783@gamil.com',
+    phone:'+91 9843076688',
+    address:{
+      line1:"57th Cross, Richmond ",
+      line2:"Circle, Church Road, London"
+    },
+    gender:'Male',
+    dob:'2005-05-28'
+  })
+
+    const [isEdit,setIsEdit] = useState(false)
+
+
   return (
-    <div>MyProfile</div>
+    <div>
+        <img src={userData.image} alt="" />
+
+      {
+        isEdit
+        ?<input type="text" value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))}/> 
+        :<p>{userData.name}</p>
+      }
+
+    </div>
   )
 }
 
